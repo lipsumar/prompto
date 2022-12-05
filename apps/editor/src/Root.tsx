dc7de7bdde4d078e1fc71fb75eccd6a6ad43bc63
Emmanuel Pire <piremmanuel@gmail.com>
2022-12-05 20:10:58 +0100

    fix api import

--- apps/editor/src/Root.tsx
+++ apps/editor/src/Root.tsx
@@ -12,7 +12,6 @@ export function Root() {
       links: [
         httpBatchLink({
           url: `${import.meta.env.VITE_API_URL}/trpc`,
-          // optional
           fetch(url, options) {
             return fetch(url, {
               ...options,
