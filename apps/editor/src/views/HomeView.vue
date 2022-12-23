977884046be6291ab2029bc206723994880bbe60
Emmanuel Pire <piremmanuel@gmail.com>
2022-12-23 14:51:41 +0100

    create project

--- apps/editor/src/views/HomeView.vue
+++ apps/editor/src/views/HomeView.vue
@@ -2,11 +2,20 @@
 import ProjectCard from "@/components/ProjectCard.vue";
 import { trpc } from "@/trpc";
 import { PlusSmallIcon } from "@heroicons/vue/20/solid";
-import { useQuery } from "vue-query";
+import { useQuery, useMutation } from "vue-query";
+import { useRouter } from "vue-router";
 
-const { data, isLoading } = useQuery("project.forUser", () =>
+const router = useRouter();
+
+const { data: projects, isLoading } = useQuery("project.forUser", () =>
   trpc.project.forUser.query()
 );
+
+const { mutate } = useMutation(() => trpc.project.create.mutate(), {
+  onSuccess(proj) {
+    router.push(`/project/${proj.id}`);
+  },
+});
 </script>
 
 <template>
@@ -15,19 +24,20 @@ const { data, isLoading } = useQuery("project.forUser", () =>
       <h1 class="text-center text-3xl my-20">Projects</h1>
       <div v-if="isLoading">Loading...</div>
       <ul class="grid grid-cols-3 gap-4">
-        <li v-for="project in data" :key="project.id">
+        <li v-for="project in projects" :key="project.id">
           <ProjectCard :title="project.name" :project-id="project.id" />
         </li>
 
         <li class="flex">
-          <div
-            class="rounded p-3 border-dashed border-2 border-slate-300 w-full flex flex-col justify-center items-center text-sm hover:border-blue-500 hover:border-solid hover:bg-white hover:text-blue-500 cursor-pointer group"
+          <button
+            class="rounded px-3 py-5 border-dashed border-2 border-slate-300 w-full flex flex-col justify-center items-center text-sm hover:border-blue-500 hover:border-solid hover:bg-white hover:text-blue-500 cursor-pointer group"
+            @click="() => mutate()"
           >
             <PlusSmallIcon
               class="h-6 w-6 text-slate-400 group-hover:text-blue-500"
             />
             New project
-          </div>
+          </button>
         </li>
       </ul>
     </div>
