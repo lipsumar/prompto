e4fa48559815d2b11571f84c36cdb5c8e341b3e0
Emmanuel Pire <piremmanuel@gmail.com>
2022-12-28 11:41:07 +0100

    dont show main when no prompt selected

--- apps/editor/src/components/editor/EditorMain.vue
+++ apps/editor/src/components/editor/EditorMain.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import { useCurrentPromptStore } from "@/stores/currentPrompt";
 import PromptEditor from "./main/PromptEditor.vue";
 import PromptOutputs from "./main/PromptOutputs.vue";
+
+const currentPromptStore = useCurrentPromptStore();
 </script>
 
 <template>
-  <div class="min-h-screen overflow-hidden">
+  <div v-if="currentPromptStore.prompt" class="min-h-screen overflow-hidden">
     <div class="h-[50vh]">
       <PromptEditor />
     </div>
