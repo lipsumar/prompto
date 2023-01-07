dbfd23047635caed01f9fa500f2f08a9f8f14748
Emmanuel Pire <piremmanuel@gmail.com>
2023-01-07 22:46:31 +0100

    splut graphEdge/graphLine

--- apps/editor/src/components/node-graph/GraphEdge.vue
+++ apps/editor/src/components/node-graph/GraphEdge.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useGraphEditorStore, type GraphEdgeData } from "@/stores/graphEditor";
 import { computed, defineProps } from "vue";
+import GraphLine from "./GraphLine.vue";
 
 const props = defineProps<{ edgeId: GraphEdgeData["id"] }>();
 const graphEditorStore = useGraphEditorStore();
@@ -20,19 +21,8 @@ const pos = computed(() => {
   const y2 = toNode.y + toNode.inputsOffset[toInputIndex].y;
   return { x1, y1, x2, y2 };
 });
-const path = computed(() => {
-  const { x1, y1, x2, y2 } = pos.value;
-  let p = `M ${x1},${y1} `;
-  // add two horizontal control points
-  const distX = x1 - x2;
-  const c1 = { x: x1 - distX / 2, y: y1 };
-  const c2 = { x: x2 + distX / 2, y: y2 };
-  p += ` C ${c1.x},${c1.y} ${c2.x},${c2.y} `;
-  p += `${x2} ${y2}`;
-  return p;
-});
 </script>
 
 <template>
-  <path class="edge" :d="path" stroke="#0ea5e9" stroke-width="4" fill="none" />
+  <GraphLine :x1="pos.x1" :y1="pos.y1" :x2="pos.x2" :y2="pos.y2" />
 </template>
---
+++ apps/editor/src/components/node-graph/GraphLine.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{ x1: number; y1: number; x2: number; y2: number }>();
+const path = computed(() => {
+  const { x1, y1, x2, y2 } = props;
+  let p = `M ${x1},${y1} `;
+  // add two horizontal control points
+  const distX = x1 - x2;
+  const c1 = { x: x1 - distX / 2, y: y1 };
+  const c2 = { x: x2 + distX / 2, y: y2 };
+  p += ` C ${c1.x},${c1.y} ${c2.x},${c2.y} `;
+  p += `${x2} ${y2}`;
+  return p;
+});
+</script>
+
+<template>
+  <path class="edge" :d="path" stroke="#0ea5e9" stroke-width="4" fill="none" />
+</template>
